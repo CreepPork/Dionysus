64c5b5b519fedc39eaff116af5c6e69a98420d04
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-07-06 23:43:26 +0300

    Added __resource.lua file

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+client_script {
+	"Dionysus.Client.net.dll",
+	"Dionysus.Shared.net.dll"
+}
+
+server_script {
+	"Dionysus.Server.net.dll",
+	"Dionysus.Shared.net.dll"
+}
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'

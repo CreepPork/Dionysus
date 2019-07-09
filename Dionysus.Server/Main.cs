1fc4579211e85ed48b1fdef3936f3a0e99e69494
Ralfs <creepporkstudios@gmail.com>
2019-07-10 00:50:52 +0300

    Added settings

--- Dionysus.Server/Main.cs
+++ Dionysus.Server/Main.cs
@@ -1,9 +1,18 @@
-﻿using CitizenFX.Core;
+﻿using System;
+using CitizenFX.Core;
 
-namespace Dionysus.Server
-{
+namespace Dionysus.Server {
     // ReSharper disable once UnusedMember.Global
-    public class Main : BaseScript
-    {
+    public class Main : BaseScript {
+        public Main() {
+            EventHandlers["onResourceStart"] += new Action(OnResourceStart);
+        }
+
+        private static void OnResourceStart() {
+            // Read JSON file for Bearer token
+            Settings settings = Settings.Get();
+            
+            Debug.WriteLine(settings.BearerToken);
+        }
     }
 }
--- __resource.lua
+++ __resource.lua
@@ -8,4 +8,8 @@ server_script {
 	"Dionysus.Shared.net.dll"
 }
 
+files {
+	'Newtonsoft.Json.dll'
+}
+
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'

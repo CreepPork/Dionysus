6462e27d6a98236e6350bd2e116ac4330a13c977
Ralfs <creepporkstudios@gmail.com>
2019-07-15 17:01:53 +0300

    Added Laravel Mix

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,1 +1,4 @@
+import settings from '../shared/settings';
+
 console.log('Client: Dionysus log');
+console.log(settings.apiUri);
---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+mix.options({
+    processCssUrls: false
+});
+
+mix.ts('src/client/client.ts', 'dist/client/client.js')
+    .ts('src/server/server.ts', 'dist/server/server.ts')
+    .setPublicPath('dist');

666c1a0306379cbb8c6c5dcb703f0c7b6e9e582a
Ralfs <creepporkstudios@gmail.com>
2019-07-17 12:41:51 +0300

    Removed unused code

--- src/client/ui/ts/app.ts
+++ src/client/ui/ts/app.ts
@@ -5,19 +5,6 @@ import './bootstrap';
 
 Vue.use(VueRouter);
 
-// Auto register all Vue components
-// @ts-ignore As this is polyfilled to a webpack require .context works.
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map((key: string) => {
-//     const splitKey = key.split('/').pop();
-//     if (splitKey !== undefined) {
-//         Vue.component(
-//             splitKey.split('.')[0],
-//             files(key).default,
-//         );
-//     }
-// });
-
 import AppLayout from './layouts/AppLayout.vue';
 import HomeView from './views/HomeView.vue';
 

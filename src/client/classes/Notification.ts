7c9646db67f73a6b251bd8e4434acd894705a8f6
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-07-18 16:03:59 +0300

    Fleshed out warning notification

--- src/client/classes/Notification.ts
+++ src/client/classes/Notification.ts
@@ -1,5 +1,8 @@
-import { EInstructionalButtonType } from "../enums/EInstructionalButtonType";
-import { EDialogKeyType } from "../enums/EDialogKeyType";
+import * as Cfx from 'fivem-js';
+
+import Delay from '../../shared/utilities/Delay';
+import { EDialogKeyType } from '../enums/EDialogKeyType';
+import { EInstructionalButtonType } from '../enums/EInstructionalButtonType';
 
 export default class Notification {
     public static DisplayHelp() {
@@ -18,59 +21,48 @@ export default class Notification {
      * Creates a fullscreen display with `Alert` type dialog.
      * Example: https://imgur.com/imwoimm
      *
+     * Allowing none of the 3 keys will result in an **infinite loop**.
+     *
      * @param messages Each line to display to the user. **Maximum is 2 lines**.
      * @param key Instructional keys to display.
+     * @param isSelectedAllowed Defines if the user can press FrontendAccept or FrontendSelect to close the dialog.
+     * @param isAlternateAllowed Defines if the user can press FrontendX (spacebar) to close the display.
+     * @param isCancelAllowed Defines if the user can press FrontendCancel to close the display.
      * @param keyPressCallback Once the key gets pressed or the user interacts with one of the controls then execute.
      */
-    public static DisplayWarning(
-        messages: string[],
+    public static async DisplayWarning(
+        firstLine: string,
+        secondLine = '',
         key: EInstructionalButtonType,
+        isSelectedAllowed: boolean,
+        isAlternateAllowed: boolean,
+        isCancelAllowed: boolean,
         // tslint:disable-next-line: no-empty
         keyPressCallback: (key: EDialogKeyType) => void = () => {},
     ) {
 
-        for (const message of messages) {
-            //
+        while (true) {
+            await Delay(5);
+
+            SetWarningMessage(firstLine, key, secondLine, false, -1, true, 0);
+
+            if (isSelectedAllowed) {
+                if (Cfx.Game.isControlJustReleased(2, Cfx.Control.FrontendAccept) ||
+                    Cfx.Game.isControlJustReleased(2, Cfx.Control.FrontendSelect)) {
+                        keyPressCallback(EDialogKeyType.Selected);
+                        break;
+                }
+            } else if (isAlternateAllowed) {
+                if (Cfx.Game.isControlJustReleased(2, Cfx.Control.FrontendX)) {
+                    keyPressCallback(EDialogKeyType.Alternate);
+                    break;
+                }
+            } else if (isCancelAllowed) {
+                if (Cfx.Game.isControlJustReleased(2, Cfx.Control.FrontendCancel)) {
+                    keyPressCallback(EDialogKeyType.Cancel);
+                    break;
+                }
+            }
         }
-
-        // AddTextEntry('warning_message_first_line')
-
-//         -- Make the first line using custom text.
-// AddTextEntry("warning_message_first_line", "This is the first line.")
-
-// -- Make the second line using custom text.
-// AddTextEntry("warning_message_second_line", "This is the second line!")
-
-// -- Add an event handler for when the screen is dismissed.
-// AddEventHandler("optionSelected", function(selected)
-//     print(selected) -- do whatever you want with the selected choice.
-//     -- players can either press the physicial buttons, or they can click
-//     -- the instructional buttons with their mouse and it will trigger
-//     -- the event as well.
-// end)
-
-
-// -- Create a thread to loop this warning message.
-// CreateThread(function()
-//     while true do
-//         Wait(0)
-//         -- Display the warning message every tick.
-//         SetWarningMessage("warning_message_first_line", 82, "warning_message_second_line", 0, -1, true, 0, 0, 0)
-
-//         -- Check for key presses or instructional button clicks.
-//         -- Input group of 2 is required for this to work while the warning is being displayed.
-
-//         if (IsControlJustReleased(2, 201) or IsControlJustReleased(2, 217)) then -- any select/confirm key was pressed.
-//             TriggerEvent("optionSelected", "select")
-//             break
-//         elseif (IsControlJustReleased(2, 203)) then -- spacebar/x on controller (alt option) was pressed.
-//             TriggerEvent("optionSelected", "alt")
-//             break
-//         elseif (IsControlJustReleased(2, 202)) then -- any of the cancel/back buttons was pressed
-//             TriggerEvent("optionSelected", "cancel")
-//             break
-//         end
-//     end
-// end)
     }
 }
